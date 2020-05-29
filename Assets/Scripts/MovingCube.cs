fd2e2a9ff70ed287cdd49f15a0ae7b8c85da2da2
Igor Sushkevich <matynoninja@gmail.com>
2020-05-30 00:06:00 +0300

    Added block trimming

--- Assets/Scripts/MovingCube.cs
+++ Assets/Scripts/MovingCube.cs
@@ -6,11 +6,16 @@ using UnityEngine;
 public class MovingCube : MonoBehaviour
 {
     public static MovingCube CurrentCube { get; private set; }
+    public static MovingCube LastCube { get; private set; }
 
+    [SerializeField]
     private float moveSpeed = 1f;
 
     private void OnEnable()
     {
+        if (LastCube == null)
+            LastCube = GameObject.Find("StartCube").GetComponent<MovingCube>();
+
         CurrentCube = this;
     }
 
@@ -18,6 +23,19 @@ public class MovingCube : MonoBehaviour
     {
         //moveSpeed = 0f;
         CurrentCube.enabled = false;
+
+        float consequance = transform.position.z - LastCube.transform.position.z;
+
+        SliceCubeOnZ(consequance);
+    }
+
+    private void SliceCubeOnZ(float hangover)
+    {
+        float newZSize = LastCube.transform.localScale.z - Mathf.Abs(hangover);
+
+        float newZPosition = LastCube.transform.position.z + (hangover / 2);
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, newZSize);
+        transform.position = new Vector3(transform.position.x, transform.position.y, newZPosition);
     }
 
     void Update()
